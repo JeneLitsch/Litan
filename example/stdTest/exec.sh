f1c6e99180fd3fe8dc6db1b09d07589021d8db26
Jene Litsch <litsch.jene@gmail.com>
2021-05-29 13:39:38 +0200

    unimportant

--- example/stdTest/exec.sh
+++ example/stdTest/exec.sh
@@ -1,3 +1,3 @@
 ./ltnc example/stdTest/test.asm.ltn example/stdTest/test.ltn
-./ltna example/stdTest/test.hex.ltn example/stdTest/test.asm.ltn -hex
-./ltnvm -run example/stdTest/test.hex.ltn -hex
+./ltna example/stdTest/test.bin.ltn example/stdTest/test.asm.ltn -bin
+./ltnvm -run example/stdTest/test.bin.ltn -bin
