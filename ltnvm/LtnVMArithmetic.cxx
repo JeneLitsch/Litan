c8a12a0b96e3f2c9cbcaa43b1158f297eec70a33
Jene Litsch <litsch.jene@gmail.com>
2021-11-23 18:11:27 +0100

    type promotion and new array "+" variants

--- ltnvm/LtnVMArithmetic.cxx
+++ ltnvm/LtnVMArithmetic.cxx
@@ -15,63 +15,142 @@ namespace ltn::vm {
 		}
 	}
 
+	struct modulo {
+		auto operator()(auto l, auto r) {
+			constexpr bool isI =
+				std::same_as<decltype(l), std::int64_t> &&
+				std::same_as<decltype(r), std::int64_t>; 
+			if(r != 0) {
+				if constexpr(isI) {
+					return l%r;
+				}
+				else {
+					return std::fmod(l, r);
+				}
+			}
+			throw std::runtime_error{"Modulo by 0"};
+		}
+	};
+
+	struct division {
+		auto operator()(auto l, auto r) {
+			using T = decltype(l/r);
+			if(r != 0) {
+				return static_cast<T>(l) / static_cast<T>(r);
+			}
+			throw std::runtime_error{"Division by 0"};
+		}
+	};
+
 	#define FETCH\
 		const auto r = this->reg.pop();\
 		const auto l = this->reg.pop();
 
-	#define BIN_II(op)\
-		if(isInt(l) && isInt(r)) {\
-			return this->reg.push(Value{l.i op r.i});\
-		}
+	std::runtime_error typeError(
+		const Value &,
+		const Value &,
+		const std::string_view & str) {
+		return std::runtime_error{ static_cast<std::string>(str) };
+	}
 
-	#define BIN_FF(op)\
-		if(isFloat(l) && isFloat(r)) {\
-			return this->reg.push(Value{l.f op r.f});\
-		}
 
-	#define BIN_FX_FF(op)\
-		if(isFloat(l) && isFloat(r)) {\
-			return this->reg.push(Value{op(l.f, r.f)});\
+	template<class OP>
+	Value arith(const Value & l, const Value & r, const std::string_view & msg) {
+		if(isBool(l)) {
+			if(isBool(r)) {
+				return { static_cast<std::int64_t>(OP()(l.b, r.b))};
+			}
+			if(isInt(r)) {
+				return {OP()(l.b, r.i)};
+			}
+			if(isFloat(r)) {
+				return {OP()(l.b, r.f)};
+			}
 		}
+		if(isInt(l)) {
+			if(isBool(r)) {
+				return {OP()(l.i, r.b)};
+			}
+			if(isInt(r)) {
+				return {OP()(l.i, r.i)};
+			}
+			if(isFloat(r)) {
+				return {OP()(l.i, r.f)};
+			}
+		}
+		if(isFloat(l)) {
+			if(isBool(r)) {
+				return {OP()(l.f, r.b)};
+			}
+			if(isInt(r)) {
+				return {OP()(l.f, r.i)};
+			}
+			if(isFloat(r)) {
+				return {OP()(l.f, r.f)};
+			}
+		}
+		throw typeError(l, r, msg);
+	}
+
+	constexpr auto arithAdd = arith<std::plus<void>>;
+	constexpr auto arithSub = arith<std::minus<void>>;
+	constexpr auto arithMlt = arith<std::multiplies<void>>;
+	constexpr auto arithDiv = arith<division>;
+	constexpr auto arithMod = arith<modulo>;
 
 	void LtnVM::add() { 
 		FETCH
-		BIN_II(+)
-		BIN_FF(+)
-		if(isArr(l) && isArr(r)) {
-			const auto arrL = this->heap.readArray(l.u);
-			const auto arrR = this->heap.readArray(r.u);
-			const auto ref = this->heap.allocArray();
-			auto & arr = this->heap.readArray(ref);
-			arr = arrL + arrR;
+		if(isArr(l)) {
+			const auto arrL = heap.readArray(l.u);
+			if(isArr(r)) {
+				const auto arrR = heap.readArray(r.u);
+				const auto ref = heap.allocArray();
+				auto & arr = heap.readArray(ref);
+				arr = arrL + arrR;
+				return this->reg.push({ref, Value::Type::ARRAY});
+			}
+			else {
+				const auto ref = heap.allocArray();
+				auto & arr = heap.readArray(ref);
+				arr = arrL;
+				arr.push_back(r);
+				return this->reg.push({ref, Value::Type::ARRAY});
+			}
+		}
+		if(isArr(r)) {
+			const auto arrR = heap.readArray(r.u);
+			const auto ref = heap.allocArray();
+			auto & arr = heap.readArray(ref);
+			arr = arrR;
+			arr.insert(arr.begin(), l);
 			return this->reg.push({ref, Value::Type::ARRAY});
 		}
-		throw std::runtime_error{"Cannot add types"};
+		this->reg.push(arithAdd(l, r, "Not operator + for types"));
 	}
+
+
+
 	void LtnVM::sub() {
 		FETCH
-		BIN_II(-)
-		BIN_FF(-)
-		throw std::runtime_error{"Cannot subtract types"};
+		this->reg.push(arithSub(l, r, "Not operator - for types"));
 	}
+
 	void LtnVM::mlt() {
 		FETCH
-		BIN_II(*)
-		BIN_FF(*)
-		throw std::runtime_error{"Cannot multiply types"};
+		this->reg.push(arithMlt(l, r, "Not operator * for types"));
 	}
+
 	void LtnVM::div() {
 		FETCH
-		BIN_II(/)
-		BIN_FF(/)
-		throw std::runtime_error{"Cannot divide types"};
+		this->reg.push(arithDiv(l, r, "Not operator / for types"));
 	}
+
 	void LtnVM::mod() {
 		FETCH
-		BIN_II(%)
-		BIN_FX_FF(std::fmod)
-		throw std::runtime_error{"Cannot mod types"};
+		this->reg.push(arithMod(l, r, "Not operator % for types"));
 	}
+
+
 
 	#undef FETCH
 	#undef BIN_II
