823d7f7880b5180f61216e937f76e9398d3e3ce0
Jene Litsch <litsch.jene@gmail.com>
2021-11-25 01:55:08 +0100

    Fixed binary evaluation order

--- ltnc/parse/parsingBinary.cxx
+++ ltnc/parse/parsingBinary.cxx
@@ -15,83 +15,93 @@ namespace ltn::c::parse {
 	// Operators * / %
 	std::unique_ptr<ast::Expression> factor(lex::Lexer & lexer) {
 		auto l = unary(lexer);
-		if(lexer.match(TT::STAR)) {
-			auto r = factor(lexer);
-			return bin(lexer, OP::MLT, l, r);
+		while(true) {
+			if(lexer.match(TT::STAR)) {
+				auto r = unary(lexer);
+				l = bin(lexer, OP::MLT, l, r);
+			}
+			else if(lexer.match(TT::SLASH)) {
+				auto r = unary(lexer);
+				l = bin(lexer, OP::DIV, l, r);
+			}
+			else if(lexer.match(TT::PERCENT)) {
+				auto r = unary(lexer);
+				l = bin(lexer, OP::MOD, l, r);
+			}
+			else return l;
 		}
-		if(lexer.match(TT::SLASH)) {
-			auto r = factor(lexer);
-			return bin(lexer, OP::DIV, l, r);
-		}
-		if(lexer.match(TT::PERCENT)) {
-			auto r = factor(lexer);
-			return bin(lexer, OP::MOD, l, r);
-		}
-		return l;
 	}
 
 	// Operators + -
 	std::unique_ptr<ast::Expression> term(lex::Lexer & lexer) {
 		auto l = factor(lexer);
-		if(lexer.match(TT::PLUS)) {
-			auto r = term(lexer);
-			return bin(lexer, OP::ADD, l, r);
+		while(true) {
+			if(lexer.match(TT::PLUS)) {
+				auto r = factor(lexer);
+				return bin(lexer, OP::ADD, l, r);
+			}
+			else if(lexer.match(TT::MINUS)) {
+				auto r = factor(lexer);
+				return bin(lexer, OP::SUB, l, r);
+			}
+			else return l;
 		}
-		if(lexer.match(TT::MINUS)) {
-			auto r = term(lexer);
-			return bin(lexer, OP::SUB, l, r);
-		}
-		return l;
 	}
 
 	// Operators << >>
 	std::unique_ptr<ast::Expression> shift(lex::Lexer & lexer) {
 		auto l = term(lexer);
-		if(lexer.match(TT::SHIFT_L)) {
-			auto r = shift(lexer);
-			return bin(lexer, OP::SHIFT_L, l, r);
+		while(true) {
+			if(lexer.match(TT::SHIFT_L)) {
+				auto r = term(lexer);
+				return bin(lexer, OP::SHIFT_L, l, r);
+			}
+			else if(lexer.match(TT::SHIFT_R)) {
+				auto r = term(lexer);
+				return bin(lexer, OP::SHIFT_R, l, r);
+			}
+			else return l;
 		}
-		if(lexer.match(TT::SHIFT_R)) {
-			auto r = shift(lexer);
-			return bin(lexer, OP::SHIFT_R, l, r);
-		}
-		return l;
 	}
 
 	// Operators < <= => >
 	std::unique_ptr<ast::Expression> comparision(lex::Lexer & lexer) {
 		auto l = shift(lexer);
-		if(lexer.match(TT::SMALLER)) {
-			auto r = comparision(lexer);
-			return bin(lexer, OP::SMALLER, l, r);
+		while(true) {
+			if(lexer.match(TT::SMALLER)) {
+				auto r = shift(lexer);
+				return bin(lexer, OP::SMALLER, l, r);
+			}
+			else if(lexer.match(TT::SMALLER_EQUAL)) {
+				auto r = shift(lexer);
+				return bin(lexer, OP::SMALLEREQUAL, l, r);
+			}
+			else if(lexer.match(TT::BIGGER)) {
+				auto r = shift(lexer);
+				return bin(lexer, OP::BIGGER, l, r);
+			}
+			else if(lexer.match(TT::BIGGER_EQUAL)) {
+				auto r = shift(lexer);
+				return bin(lexer, OP::BIGGEREQUAL, l, r);
+			}
+			else return l;
 		}
-		if(lexer.match(TT::SMALLER_EQUAL)) {
-			auto r = comparision(lexer);
-			return bin(lexer, OP::SMALLEREQUAL, l, r);
-		}
-		if(lexer.match(TT::BIGGER)) {
-			auto r = comparision(lexer);
-			return bin(lexer, OP::BIGGER, l, r);
-		}
-		if(lexer.match(TT::BIGGER_EQUAL)) {
-			auto r = comparision(lexer);
-			return bin(lexer, OP::BIGGEREQUAL, l, r);
-		}
-		return l;
 	}
 
 	// Operators == != 
 	std::unique_ptr<ast::Expression> equality(lex::Lexer & lexer) {
 		auto l = comparision(lexer);
-		if(lexer.match(TT::EQUAL)) {
-			auto r = equality(lexer);
-			return bin(lexer, OP::EQUAL, l, r);
+		while(true) {
+			if(lexer.match(TT::EQUAL)) {
+				auto r = comparision(lexer);
+				return bin(lexer, OP::EQUAL, l, r);
+			}
+			else if(lexer.match(TT::UNEQUAL)) {
+				auto r = comparision(lexer);
+				return bin(lexer, OP::UNEQUEL, l, r);
+			}
+			else return l;
 		}
-		if(lexer.match(TT::UNEQUAL)) {
-			auto r = equality(lexer);
-			return bin(lexer, OP::UNEQUEL, l, r);
-		}
-		return l;
 	}
 
 }
