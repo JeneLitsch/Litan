82607c1dc7ec17c517a1b738673aca9a6316e475
Jene Litsch <litsch.jene@gmail.com>
2021-11-29 21:35:59 +0100

    test/fx.ltn

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,13 @@
 run () {
+	echo "Testing \"$1\""
 	ltnFile="test/$1.ltn"
 	asmFile="test/$1.asm.ltn"
 	binFile="test/$1.bin.ltn"
 	./bin/ltnc 	$asmFile ./stdlib $ltnFile
 	./bin/ltna 	$binFile  $asmFile
 	./bin/ltnvm $binFile
+	echo 
 }
 
 run "vars"
+run "fx"
