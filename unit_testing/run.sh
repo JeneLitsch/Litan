e50649ceebb2e86c62c6a56fc24a5fd75a211ce2
Jene Litsch <litsch.jene@gmail.com>
2022-02-19 19:18:45 +0100

    More tests

--- unit_testing/run.sh
+++ unit_testing/run.sh
@@ -5,7 +5,10 @@ binFile="unit_testing/$test.bin.ltn"
 ./bin/ltnc 	$asmFile ./stdlib\
 	unit_testing/utils.ltn\
 	unit_testing/main.ltn\
-	unit_testing/math.ltn\
+	unit_testing/arith.ltn\
+	unit_testing/comp.ltn\
+	unit_testing/collection.ltn\
+	unit_testing/exception.ltn\
 
 ./bin/ltna 	$binFile  $asmFile
 ./bin/ltnvm $binFile
