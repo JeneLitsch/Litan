f01e3c767ab9607fade2ff51803cc107b816cbc4
Jene Litsch <litsch.jene@gmail.com>
2022-02-19 14:28:51 +0100

    Minor refactoring

--- ltn/InstructionSet.hxx
+++ ltn/InstructionSet.hxx
@@ -2,36 +2,130 @@
 #include <cstdint>
 namespace ltn {
 	enum class Inst : std::uint8_t {
-		EXIT = 0x00, ERROR, STATE, TRY, THROW,
-		ADD = 0x10, SUB, MLT, DIV, MOD,
-		EQL, UEQL, SML, BGR, SMLEQL, BGREQL,
-		SHIFT_L, SHIFT_R,
-
-		NEG = 0x20, NOT, INC, DEC,
+		// System
+		EXIT = 0x00,
+		ERROR,
+		STATE,
+		TRY,
+		THROW,
 		
-		NEWI = 0x30, NEWF, NEWU, NEWC, TRUE, FALSE, NVLL,
+		// Binary arithmetic
+		ADD = 0x10,
+		SUB,
+		MLT,
+		DIV,
+		MOD,
 
-		JUMP = 0x40, CALL, RETURN, IF, INVOKE, EXTERNAL, ALGORITHM, CAPTURE, PARAMETERS,
+		// Comparisons
+		EQL = 0x18,
+		UEQL,
+		SML,
+		BGR,
+		SMLEQL,
+		BGREQL,
+		
+		// Shift
+		SHIFT_L,
+		SHIFT_R,
 
-		NEWARR = 0x50, NEWSTR, NEWOUT, NEWIN, NEWFX, NEWCLOCK, NEWSTRUCT, NEWRANGE, NEWSTACK, NEWQUEUE, NEWMAP,
+		// Unary operators
+		NEG = 0x20,
+		NOT,
+		INC,
+		DEC,
+		
+		// Primitives
+		NEWI = 0x30,
+		NEWF,
+		NEWU,
+		NEWC,
+		TRUE,
+		FALSE,
+		NVLL,
 
-		READ = 0x60, WRITE, SCRAP, MAKEVAR,  READ_X, WRITE_X, SWAP,
-		READ_0 = 0x68, READ_1, READ_2, READ_3, 
-		WRITE_0, WRITE_1, WRITE_2, WRITE_3,
+		// Controlflow and functions
+		JUMP = 0x40,
+		CALL,
+		RETURN,
+		IF,
+		INVOKE,
+		EXTERNAL,
+		ALGORITHM,
+		CAPTURE,
+		PARAMETERS,
 
-		OUT = 0x70, IN_STR, IN_LINE, IN_INT, IN_FLOAT, IS_EOF,
+		// Allocations
+		NEWARR = 0x50,
+		NEWSTR,
+		NEWOUT,
+		NEWIN,
+		NEWFX,
+		NEWCLOCK,
+		NEWSTRUCT,
+		NEWRANGE,
+		NEWSTACK,
+		NEWQUEUE,
+		NEWMAP,
 
-		MIN = 0x80, MAX, ROUND, FLOOR, CEIL, ABS, HYPOT, SQRT, LOG, LN, POW,
-		SIN = 0x90, COS, TAN, 
+		// Stack reading
+		READ = 0x60,
+		WRITE,
+		SCRAP,
+		MAKEVAR,
+		READ_X,
+		WRITE_X,
+		SWAP,
 
+		// Fast stack reading
+		READ_0 = 0x68,
+		READ_1,
+		READ_2,
+		READ_3, 
+		WRITE_0,
+		WRITE_1,
+		WRITE_2,
+		WRITE_3,
+
+		// IO
+		OUT = 0x70,
+		IN_STR,
+		IN_LINE,
+		IN_INT,
+		IN_FLOAT,
+		IS_EOF,
+
+		// Math
+		MIN = 0x80,
+		MAX,
+		ROUND,
+		FLOOR,
+		CEIL,
+		ABS,
+		HYPOT,
+		SQRT,
+		LOG,
+		LN,
+		POW,
+		SIN,
+		COS,
+		TAN, 
+
+		// Collections and containers
 		SIZE = 0xa0,
-		AT, AT_WRITE,
-		FRONT, BACK, 
-		INSERT, REMOVE,
-		BEGIN, END,
-		PUSH, POP, PEEK,
+		AT,
+		AT_WRITE,
+		FRONT,
+		BACK, 
+		INSERT,
+		REMOVE,
+		BEGIN,
+		END,
+		PUSH,
+		POP,
+		PEEK,
 		CONTAINS,
 
+		// Cast
 		CAST_BOOL = 0xb0,
 		CAST_CHAR,
 		CAST_INT,
@@ -39,8 +133,12 @@ namespace ltn {
 		CAST_STRING,
 		CAST_ARRAY,
 
-		TYPEID = 0xc0, CLONE,
+		// Type utils
+		TYPEID = 0xc0,
+		CLONE,
 
-		MEMBER_READ = 0xd0, MEMBER_WRITE,		
+		// Structs
+		MEMBER_READ = 0xd0,
+		MEMBER_WRITE,		
 	};
 }
