cf3dc1c24eac52cf16a9e280c8c479910c49f346
Jene Litsch <litsch.jene@gmail.com>
2022-02-26 14:25:20 +0100

    Lambda capture test

--- test/run.sh
+++ test/run.sh
@@ -3,22 +3,23 @@ asmFile="test/$test.asm.ltn"
 binFile="test/$test.bin.ltn"
 
 ./bin/ltnc 	$asmFile ./stdlib\
-	test/bool.ltn\
-	test/utils.ltn\
-	test/main.ltn\
-	test/arith.ltn\
-	test/compare.ltn\
-	test/array.ltn\
-	test/queue.ltn\
-	test/stack.ltn\
-	test/math.ltn\
-	test/exception.ltn\
-	test/range.ltn\
 	test/algorithm.ltn\
+	test/arith.ltn\
+	test/array.ltn\
+	test/bool.ltn\
+	test/compare.ltn\
+	test/captures.ltn\
+	test/exception.ltn\
+	test/loops.ltn\
+	test/main.ltn\
+	test/math.ltn\
+	test/namespace.ltn\
+	test/queue.ltn\
+	test/range.ltn\
+	test/stack.ltn\
 	test/struct.ltn\
 	test/types.ltn\
-	test/loops.ltn\
-	test/namespace.ltn\
+	test/utils.ltn\
 
 ./bin/ltna 	$binFile  $asmFile
 ./bin/ltnvm $binFile
