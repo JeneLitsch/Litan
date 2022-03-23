dfc3feae1bb6fe00738aeb07a78b76c70ef7a8e0
Jene Litsch <litsch.jene@gmail.com>
2022-03-23 19:20:37 +0100

    null literal returns else brach of ternary op immediately

--- ltnc_core/optimize/optimize_ternary.cxx
+++ ltnc_core/optimize/optimize_ternary.cxx
@@ -2,38 +2,55 @@
 #include "ltn/casts.hxx"
 #include "eval/eval_unary.hxx"
 namespace ltn::c::optimize {
-	ast::expr_ptr ternary(ast::Ternary & expr) {
-		expr.condition = expression(std::move(expr.condition));
-		expr.if_expr   = expression(std::move(expr.if_expr));
-		expr.else_expr = expression(std::move(expr.else_expr));
-
-		if(auto * litr = as<ast::Bool>(*expr.condition)) {
-			if(litr->value) return std::move(expr.if_expr);
-			else return std::move(expr.else_expr);
+	template<typename ConditionNode>
+	ast::expr_ptr decide(ast::Ternary & expr) {
+		if(auto * litr = as<ConditionNode>(*expr.condition)) {
+			if(litr->value) {
+				return std::move(expr.if_expr);
+			}
+			else {
+				return std::move(expr.else_expr);
+			}
 		}
+		return nullptr;
+	} 
 
-		if(auto * litr = as<ast::Char>(*expr.condition)) {
-			if(litr->value) return std::move(expr.if_expr);
-			else return std::move(expr.else_expr);
-		}
-
-		if(auto * litr = as<ast::Integer>(*expr.condition)) {
-			if(litr->value) return std::move(expr.if_expr);
-			else return std::move(expr.else_expr);
-		}
-
-		if(auto * litr = as<ast::Float>(*expr.condition)) {
-			if(litr->value) return std::move(expr.if_expr);
-			else return std::move(expr.else_expr);
-		}
-
+	template<>
+	ast::expr_ptr decide<ast::String>(ast::Ternary & expr) {
 		if(auto * litr = as<ast::String>(*expr.condition)) {
 			return std::move(expr.if_expr);
 		}
+		return nullptr;
+	} 
 
-		if(auto * litr = as<ast::Array>(*expr.condition)) {
+	template<>
+	ast::expr_ptr decide<ast::Array>(ast::Ternary & expr) {
+		if(auto * litr = as<ast::String>(*expr.condition)) {
 			return std::move(expr.if_expr);
 		}
+		return nullptr;
+	} 
+
+	template<>
+	ast::expr_ptr decide<ast::Null>(ast::Ternary & expr) {
+		if(auto * litr = as<ast::String>(*expr.condition)) {
+			return std::move(expr.else_expr);
+		}
+		return nullptr;
+	} 
+
+	ast::expr_ptr ternary(ast::Ternary & ternary) {
+		ternary.condition = expression(std::move(ternary.condition));
+		ternary.if_expr   = expression(std::move(ternary.if_expr));
+		ternary.else_expr = expression(std::move(ternary.else_expr));
+
+		if(auto expr = decide<ast::Bool>(ternary))    return expr;
+		if(auto expr = decide<ast::Char>(ternary))    return expr;
+		if(auto expr = decide<ast::Integer>(ternary)) return expr;
+		if(auto expr = decide<ast::Float>(ternary))   return expr;
+		if(auto expr = decide<ast::Float>(ternary))   return expr;
+		if(auto expr = decide<ast::String>(ternary))  return expr;
+		if(auto expr = decide<ast::Array>(ternary))   return expr;
 
 		return nullptr;
 	}
