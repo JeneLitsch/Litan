bc2c82694993f304fb309e8ea1ef76ff1e8d515c
Jene Litsch <litsch.jene@gmail.com>
2022-03-23 19:09:50 +0100

    Pre-evaluation of ternary operator

--- ltnc_core/optimize/optimize_ternary.cxx
+++ ltnc_core/optimize/optimize_ternary.cxx
@@ -1,10 +1,40 @@
 #include "optimize.hxx"
 #include "ltn/casts.hxx"
+#include "eval/eval_unary.hxx"
 namespace ltn::c::optimize {
 	ast::expr_ptr ternary(ast::Ternary & expr) {
 		expr.condition = expression(std::move(expr.condition));
 		expr.if_expr   = expression(std::move(expr.if_expr));
 		expr.else_expr = expression(std::move(expr.else_expr));
+
+		if(auto * litr = as<ast::Bool>(*expr.condition)) {
+			if(litr->value) return std::move(expr.if_expr);
+			else std::move(expr.else_expr);
+		}
+
+		if(auto * litr = as<ast::Char>(*expr.condition)) {
+			if(litr->value) return std::move(expr.if_expr);
+			else std::move(expr.else_expr);
+		}
+
+		if(auto * litr = as<ast::Integer>(*expr.condition)) {
+			if(litr->value) return std::move(expr.if_expr);
+			else std::move(expr.else_expr);
+		}
+
+		if(auto * litr = as<ast::Float>(*expr.condition)) {
+			if(litr->value) return std::move(expr.if_expr);
+			else std::move(expr.else_expr);
+		}
+
+		if(auto * litr = as<ast::String>(*expr.condition)) {
+			return std::move(expr.if_expr);
+		}
+
+		if(auto * litr = as<ast::Array>(*expr.condition)) {
+			return std::move(expr.if_expr);
+		}
+
 		return nullptr;
 	}
 }
