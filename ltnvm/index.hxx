8532add95a2987c64a10ae137c309e2501a03057
Jene Litsch <litsch.jene@gmail.com>
2022-05-17 21:06:08 +0200

    Moved remaining tests to sphinx

--- ltnvm/index.hxx
+++ ltnvm/index.hxx
@@ -6,16 +6,16 @@
 namespace ltn::vm {
 	void guard_index(const auto & collection, auto i) {
 		if(i < 0) {
-			throw std::runtime_error{"Negative index is not allowed"};
+			throw Exception{Exception::Type::OUT_OF_RANGE, "Negative index is not allowed"};
 		}
 		if(i >= static_cast<decltype(i)>(collection.size())) {
-			throw std::runtime_error{"Index out of range"};
+			throw Exception{Exception::Type::OUT_OF_RANGE, "Index out of range"};
 		}
 	}
 
 	inline std::int64_t to_index(const Value value) {
 		if(!is_int(value)) {
-			throw std::runtime_error{"Expected integer as index"};
+			throw Exception{Exception::Type::OUT_OF_RANGE,"Expected integer as index"};
 		}
 		return value.i;
 	}
