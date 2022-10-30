a08ec25eceb1a147ac83e6f264f8cc271702816c
Jene Litsch <litsch.jene@gmail.com>
2022-10-30 12:29:26 +0100

    Rewritten tests for -

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,7 @@
 files=(
 	"test/main.ltn"
 	"test/op_add.ltn"
+	"test/op_sub.ltn"
 	"test/sphinx/sphinx.ltn" 
 )
 
@@ -9,8 +10,6 @@ asm_file="tmp/asm"
 
 rm $bin_file
 rm $asm_file
-
-# ./bin/ltn --src ${core_tests[*]} ${stdlib_tests[*]} ${misc[*]} --args Hello World 123
 
 ./bin/ltnc -o --asm $asm_file --exe $bin_file --src ${files[*]}
 ./bin/ltnvm --exe $bin_file --args Hello World 123
