547276a38af9622a5193c7aaf8d56e190b6862a2
Jene Litsch <litsch.jene@gmail.com>
2022-11-30 12:50:20 +0100

    static cast for char and float

--- ltnc/compile/cast_static.cxx
+++ ltnc/compile/cast_static.cxx
@@ -14,8 +14,16 @@ namespace ltn::c {
 				return { type_code::BOOL };
 			}
 
+			if(type::is_numeric(from) && type::is_char(to)) {
+				return { type_code::CHAR };
+			}
+
 			if(type::is_numeric(from) && type::is_int(to)) {
 				return { type_code::INT };
+			}
+
+			if(type::is_numeric(from) && type::is_float(to)) {
+				return { type_code::FLOAT };
 			}
 
 			throw cannot_cast(from, to, location);
