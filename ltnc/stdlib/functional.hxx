b506f097127777ebefea3f3913ebe3cca72f06a0
Jene Litsch <litsch.jene@gmail.com>
2022-12-01 21:46:08 +0100

    Added some annotation to stdlib

--- ltnc/stdlib/functional.hxx
+++ ltnc/stdlib/functional.hxx
@@ -1,17 +1,20 @@
 #pragma once
 inline const char * std_functional = R"###(
 namespace std {
-	build_in arity(id)            @ functional_arity
+	build_in arity(id) const -> int
+		@ functional_arity
 
-	build_in external(id)         @ functional_external
+	build_in external(id)
+		@ functional_external
 	
-	build_in invoke(fx, params)   @ functional_invoke_2
+	build_in invoke(fx, params)
+		@ functional_invoke_2
 	
-	function chain(f1, f2)
+	function chain(f1, f2) const
 		=> lambda [f1, f2] (x) 
 			=> (f2)((f1)(x))
 
-	function is_successful(fx) {
+	function is_successful(fx) -> bool {
 		fx();
 		return true;
 	}
