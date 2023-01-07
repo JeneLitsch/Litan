0c96f9fee36b24aabd516f42dcbdbbb6b44d0d11
Jene Litsch <litsch.jene@gmail.com>
2023-01-07 10:58:18 +0100

    Added more type annotations to stdlib

--- ltnc/stdlib/random.hxx
+++ ltnc/stdlib/random.hxx
@@ -2,14 +2,26 @@
 inline const char * std_random = R"###(
 namespace std {
 	namespace random {
-		build_in mersenne()                @ random_mersenne_0
-		build_in mersenne(seed) const      @ random_mersenne_1
-		build_in rand(rng)                 @ random_rand
-		build_in rand_int(rng, min, max)   @ random_rand_int
-		build_in rand_float(rng, min, max) @ random_rand_float
-		build_in split(rng)                @ random_split
+		build_in mersenne()
+			@ random_mersenne_0
+		
+		build_in mersenne(seed) const
+			@ random_mersenne_1
+		
+		build_in rand(rng) -> int
+			@ random_rand
+		
+		build_in rand_int(rng, min, max) -> int  
+			@ random_rand_int
+		
+		build_in rand_float(rng, min, max) -> float
+			@ random_rand_float
+		
+		build_in split(rng)
+			@ random_split
 
-		function decision(rng, p) => rand_float(rng, 0, 1) < p
+		function decision(rng, p) -> bool
+			=> rand_float(rng, 0, 1) < p
 	}
 }
 )###";
