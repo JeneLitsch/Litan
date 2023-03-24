b4047b88a1819a40a4c6025c56d6cfa94aba6674
Jene Litsch <litsch.jene@gmail.com>
2023-03-24 21:15:28 +0100

    Refactored ltn::build(...)

--- Litan.cxx
+++ Litan.cxx
@@ -5,10 +5,8 @@
 namespace ltn {
 	std::vector<std::uint8_t> build(const std::vector<ltn::c::Source> & user_sources) {
 		ltn::c::Reporter reporter;
-		auto sources = ltn::c::read_sources(std::vector<std::string>{}, reporter);
-		for(auto & s : user_sources) {
-			sources.push_back(s);
-		}
+		auto sources = user_sources;
+		ltn::c::inject_stdlib(sources);
 		auto tokens = ltn::c::lex(sources, reporter);
 		auto ast = ltn::c::parse(tokens, reporter);
 		auto sst = ltn::c::analyze(ast, reporter);
