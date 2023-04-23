08a50d72b76379d2a2ec5cbbe751f012eb17d3e7
Jene Litsch <litsch.jene@gmail.com>
2023-04-24 01:24:59 +0200

    Added std::range() with default step = 1

--- ltnc/stdlib/iter.hxx
+++ ltnc/stdlib/iter.hxx
@@ -2,6 +2,7 @@
 inline const char * std_iter = R"###(
 	namespace std {
 		build_in range(begin : int, end : int, step : int) @iter_range
+		function range(begin : int, end : int) => range(begin, end, 1)
 		build_in next(iterator) @iter_next
 		build_in iter(iterator) @iter_iter
 	}
