e139e84cfcc2f69474dd7909c84ffe492e22c6ac
Jene Litsch <litsch.jene@gmail.com>
2023-04-25 17:28:28 +0200

    Added doc for std::iter::get

---
+++ doc/stdlib/iter/get.md
@@ -0,0 +1,12 @@
+# std::iter::get
+
+## std::iter::get(iterator)
+**Parameters:**
+- iterator: any iterator
+
+**Return value:**  
+any
+
+**Description:**  
+Returns the current value.
+Returns an iterator_stop value if the iterator reached the end.
--- sandbox/run.sh
+++ sandbox/run.sh
@@ -4,6 +4,8 @@ run () {
 	asmFile="sandbox/$1.asm.ltn"
 	binFile="sandbox/$1.bin.ltn"
 	./bin/ltnc -o --exe $binFile --asm $asmFile --src $ltnFile
+	./bin/ltnvm_0 --exe $binFile --args Hello World 123
+	./bin/ltnvm_1 --exe $binFile --args Hello World 123
 	./bin/ltnvm --exe $binFile --args Hello World 123
 	echo 
 }
