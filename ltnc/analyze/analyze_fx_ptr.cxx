7d1512c403354c0516a2de6c71275076b5c63f38
Jene Litsch <litsch.jene@gmail.com>
2023-04-26 12:29:54 +0200

    Deduced parameter types of function pointer take on default type any

--- ltnc/analyze/analyze_fx_ptr.cxx
+++ ltnc/analyze/analyze_fx_ptr.cxx
@@ -5,34 +5,35 @@
 
 namespace ltn::c {
 	namespace {
-		std::vector<type::Type> instantiate_parameters(
-			const ast::Parameters & parameters,
+
+		struct ParamResult {
+			type::Type type;
+			std::optional<std::pair<std::string, type::Type>> deduced_type;
+		};
+
+
+		ParamResult analyze_parameter(
+			const ast::Parameter::Infered & infered,
+			std::optional<type::IncompleteType> declared,
 			Scope & scope) {
-			
-			return stx::fx::map([&] (const auto & parameter) {
-				return analyze_parameter_type(parameter.type, scope);
-			}, parameters);
+			auto type = declared.value_or(type::IncompleteType{type::Any{}});
+			return ParamResult{
+				.type = instantiate_type(type, scope),
+				.deduced_type = std::make_pair(infered.name, instantiate_type(type, scope))
+			}; 
 		}
 
 
 
-
-		auto resolve_fx_ptr(const ast::FxPointer & fx_ptr, Scope & scope) {
-			auto & context = scope.get_context();
-
-			const auto fx = scope.resolve_function(
-				fx_ptr.name,
-				fx_ptr.namespaze,
-				fx_ptr.placeholders
-			);
-
-			if(!fx) throw undefined_function(fx_ptr);
-
-			context.fx_queue.stage_function(*fx, {});
-
-			const auto label = make_function_label(*fx);
-
-			return std::tuple{fx, label};
+		ParamResult analyze_parameter(
+			const type::IncompleteType & type,
+			std::optional<type::IncompleteType> declared,
+			Scope & scope) {
+				
+			return ParamResult{
+				.type = instantiate_type(type, scope),
+				.deduced_type = std::nullopt
+			}; 
 		}
 	}
 
@@ -41,22 +42,58 @@ namespace ltn::c {
 	sst::expr_ptr analyze_expr(const ast::FxPointer & fx_ptr, Scope & scope) {
 
 		MinorScope inner_scope{&scope};
-		auto [funtional, label] = resolve_fx_ptr(fx_ptr, inner_scope);
+
+		std::map<std::string, type::Type> deduced_types;
+		std::vector<type::Type> parameter_types;
+
+		auto & context = scope.get_context();
+
+		const auto fx = scope.resolve_function(
+			fx_ptr.name,
+			fx_ptr.namespaze,
+			fx_ptr.placeholders
+		);
+
+		if(!fx) throw undefined_function(fx_ptr);
+
+
+
+
+		
+		for(const auto & param : fx->parameters) {
+			auto [type, deduced] = std::visit([&] (auto & p) {
+				return analyze_parameter(p, std::nullopt, scope);
+			}, param.type);
+			if(deduced) {
+				deduced_types.insert(*deduced);
+			}
+			parameter_types.push_back(type);
+		}
+
+		MinorScope param_scope{&inner_scope};
+
+		param_scope.inherit_types(deduced_types);
 
 		const auto return_type 
-			= instantiate_type(funtional->return_type, inner_scope);
-		
-		const auto parameter_types
-			= instantiate_parameters(funtional->parameters, inner_scope);
+			= instantiate_type(fx->return_type, param_scope);
 
 		const auto type = type::FxPtr {
 			.return_type = return_type,
 			.parameter_types = parameter_types,
 		};
 
+		context.fx_queue.stage_function(Staged{
+			.fx = *fx,
+			.deduced_types = deduced_types,
+			.captures = {},
+			.override_namespace = std::nullopt,
+		});
+
+		const auto label = make_function_label(*fx, deduced_types);
+
 		return std::make_unique<sst::FxPointer>(
 			label,
-			funtional->parameters.size(),
+			fx->parameters.size(),
 			type
 		);
 	}
