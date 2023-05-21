13a77f17de4fd1b1083b4445e4c7965bb4a1d1ba
Jene Litsch <litsch.jene@gmail.com>
2023-05-21 22:44:08 +0200

    Doc: std::zipped

---
+++ doc/stdlib/iter/zipped.md
@@ -0,0 +1,11 @@
+# std::iter::zipped
+
+## std::iter::zipped(ranges) const
+**Parameters:**
+- iters: tuple of iterable values
+
+**Return value:**  
+iterator
+
+**Description:**  
+Creates a zipped iterator.
--- ltnc/stdlib/iter.hxx
+++ ltnc/stdlib/iter.hxx
@@ -3,16 +3,16 @@ inline const char * std_iter = R"###(
 	namespace std {
 		build_in range(begin, end, step) const @iter_range
 		function range(begin, end) const => range(begin, end, 1)
-		build_in zipped(iters) @iter_zipped 
+		build_in zipped(iters) const @iter_zipped 
 		namespace iter {
 			build_in range(begin, end, step) const @iter_range
 			function range(begin, end) const => range(begin, end, 1)
 			build_in next(iterator) @iter_next
-			build_in get(iterator) @iter_get
+			build_in get(iterator) const @iter_get
 			build_in move(iterator, steps) @iter_move
 			build_in iter(iterator) const @iter_iter
 			build_in is_stop(value) const @iter_is_stop
-			build_in zipped(iters) @iter_zipped 
+			build_in zipped(iters) const @iter_zipped 
 		}
 	}
 )###";
