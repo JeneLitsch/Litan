e76b41cbb0a71df181f191f4284a9f21f9e46c9f
Jene Litsch <litsch.jene@gmail.com>
2023-06-02 18:48:59 +0200

    Change: Rewritten std::join

--- ltnc/stdlib/string.hxx
+++ ltnc/stdlib/string.hxx
@@ -4,8 +4,8 @@ namespace std {
 	build_in split(string, delim) const 
 		@split_string
 
-	build_in join(strings, joiner) const
-		@join_string
+	function join(strings, joiner) const
+		=> std::reduce_l(strings, lambda[joiner](a,b) => std::str(a) + joiner + std::str(b)) ?? ""
 
 	function replace(string, search_for, replace_with) const
 		=> (search_for == "" && replace_with == "") 
