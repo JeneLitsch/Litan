2a20fa0818048d8505c063a5b4bfe3a5db4b640a
Jene Litsch <litsch.jene@gmail.com>
2023-06-21 13:20:13 +0200

    Fix: Fixed transpiler indentation

--- ltnc/transpile/c/transpile_c.cxx
+++ ltnc/transpile/c/transpile_c.cxx
@@ -11,35 +11,35 @@ namespace ltn::c::trans::cxx {
 
 
 
-		void print_value_type_enum(std::ostream & stream) {
-			stream << "enum ValueType {\n";
-			stream << "\tNVLL = 0x00,\n";
-			stream << "\tBOOL = 0x10, INT, FLOAT, CHAR,\n";
-			stream << "\tARRAY = 0x20, STRING, TUPLE,\n";
-			stream << "\tISTREAM = 0x30, OSTREAM,\n";
-			stream << "\tFX_PTR = 0x40, ITERATOR, ITERATOR_STOP,\n";
-			stream << "\tCLOCK = 0x50,\n";
-			stream << "\tSTRUCT = 0x60,\n";
-			stream << "\tQUEUE = 0x70, STACK, MAP,\n";
-			stream << "\tRNG = 0x80, TYPE,\n";
-			stream << "};\n";
-			stream << "\n";
+		void print_value_type_enum(std::ostream & stream, Indent indent) {
+			stream << indent << "enum ValueType {\n";
+			stream << indent.in() << "NVLL = 0x00,\n";
+			stream << indent.in() << "BOOL = 0x10, INT, FLOAT, CHAR,\n";
+			stream << indent.in() << "ARRAY = 0x20, STRING, TUPLE,\n";
+			stream << indent.in() << "ISTREAM = 0x30, OSTREAM,\n";
+			stream << indent.in() << "FX_PTR = 0x40, ITERATOR, ITERATOR_STOP,\n";
+			stream << indent.in() << "CLOCK = 0x50,\n";
+			stream << indent.in() << "STRUCT = 0x60,\n";
+			stream << indent.in() << "QUEUE = 0x70, STACK, MAP,\n";
+			stream << indent.in() << "RNG = 0x80, TYPE,\n";
+			stream << indent << "};\n";
+			stream << indent << "\n";
 		}
 
 
 
-		void print_value_struct(std::ostream & stream) {
-			stream << "struct Value {\n";
-			stream << "\t" << "union Val {\n";
-			stream << "\t\t" << "uint8_t b;\n";
-			stream << "\t\t" << "uint8_t c;\n";
-			stream << "\t\t" << "int64_t i;\n";
-			stream << "\t\t" << "double f;\n";
-			stream << "\t" << "};\n";
-			stream << "\t" << "Value(ValueType type, Val val) : type{type}, val{val} {}\n";
-			stream << "\t" << "uint32_t type;\n";
-			stream << "\t" << "Val val;\n";
-			stream << "};\n\n";
+		void print_value_struct(std::ostream & stream, Indent indent) {
+			stream << indent << "struct Value {\n";
+			stream << indent.in() << "union Val {\n";
+			stream << indent.in().in() << "uint8_t b;\n";
+			stream << indent.in().in() << "uint8_t c;\n";
+			stream << indent.in().in() << "int64_t i;\n";
+			stream << indent.in().in() << "double f;\n";
+			stream << indent.in() << "};\n";
+			stream << indent.in() << "Value(ValueType type, Val val) : type{type}, val{val} {}\n";
+			stream << indent.in() << "uint32_t type;\n";
+			stream << indent.in() << "Val val;\n";
+			stream << indent << "};\n\n";
 		}
 
 
@@ -53,23 +53,24 @@ namespace ltn::c::trans::cxx {
 
 
 
-		void print_value_null(std::ostream & stream) {
-			stream << "struct Value value_null() {\n";
-			stream << "\t" << "return Value {NVLL, Value::Val {} };\n";
-			stream << "}\n\n";
+		void print_value_null(std::ostream & stream, Indent indent) {
+			stream << indent << "struct Value value_null() {\n";
+			stream << indent.in() << "\t" << "return Value {NVLL, Value::Val {} };\n";
+			stream << indent << "}\n\n";
 		}
 
 
 		void print_value_xyz(
 			std::ostream & stream,
+			Indent indent,
 			std::string_view name,
 			std::string_view ltn_type,
 			std::string_view c_type,
 			std::string_view member) {
 			
-			stream << "struct Value value_" << name << "(" << c_type << " x) {\n";
-			stream << "\t" << "return Value {" << ltn_type << ", Value::Val { ." << member << " = x } };\n";
-			stream << "}\n\n";
+			stream << indent << "struct Value value_" << name << "(" << c_type << " x) {\n";
+			stream << indent.in() << "return Value {" << ltn_type << ", Value::Val { ." << member << " = x } };\n";
+			stream << indent << "}\n\n";
 		}
 
 
@@ -90,61 +91,66 @@ namespace ltn::c::trans::cxx {
 		void print_wrapped_operator(
 			std::ostream & stream,
 			std::string_view name,
-			std::string_view op) {
+			std::string_view op,
+			Indent indent) {
 
-			stream << "Value " << name << "_impl(std::integral auto l, std::integral auto r) {\n";
-			stream << "\t auto x = static_cast<std::int64_t>(l)" << op << "static_cast<std::int64_t>(r);\n";
-			stream << "\t return value_int(x);\n";
-			stream << "}\n\n";
+			auto indent_1 = indent.in();
+			
+			stream << indent << "Value " << name << "_impl(std::integral auto l, std::integral auto r) {\n";
+			stream << indent_1 << "auto x = static_cast<std::int64_t>(l)" << op << "static_cast<std::int64_t>(r);\n";
+			stream << indent_1 << "return value_int(x);\n";
+			stream << indent << "}\n\n";
 
-			stream << "Value " << name << "_impl(std::integral auto l, std::floating_point auto r) {\n";
-			stream << "\t auto x = static_cast<double>(l)" << op << "static_cast<double>(r);\n";
-			stream << "\t return value_float(x);\n";
-			stream << "}\n\n";
+			stream << indent << "Value " << name << "_impl(std::integral auto l, std::floating_point auto r) {\n";
+			stream << indent_1 << "auto x = static_cast<double>(l)" << op << "static_cast<double>(r);\n";
+			stream << indent_1 << "return value_float(x);\n";
+			stream << indent << "}\n\n";
 
-			stream << "Value " << name << "_impl(std::floating_point auto l, std::integral auto r) {\n";
-			stream << "\t auto x = static_cast<double>(l)" << op << "static_cast<double>(r);\n";
-			stream << "\t return value_float(x);\n";
-			stream << "}\n\n";
+			stream << indent << "Value " << name << "_impl(std::floating_point auto l, std::integral auto r) {\n";
+			stream << indent_1 << "auto x = static_cast<double>(l)" << op << "static_cast<double>(r);\n";
+			stream << indent_1 << "return value_float(x);\n";
+			stream << indent << "}\n\n";
 
-			stream << "Value " << name << "_impl(std::floating_point auto l, std::floating_point auto r) {\n";
-			stream << "\t auto x = static_cast<double>(l)" << op << "static_cast<double>(r);\n";
-			stream << "\t return value_float(x);\n";
-			stream << "}\n\n";
+			stream << indent << "Value " << name << "_impl(std::floating_point auto l, std::floating_point auto r) {\n";
+			stream << indent_1 << "auto x = static_cast<double>(l)" << op << "static_cast<double>(r);\n";
+			stream << indent_1 << "return value_float(x);\n";
+			stream << indent << "}\n\n";
 		}
 
 
 
-		void print_arith_dispatch(std::ostream & stream, std::string_view name) {
-			
-			stream << "Value " << name << "(const Value & l, const Value & r){\n";
-			stream << "\t" << "switch(l.type) {\n";
-			stream << "\t\t" << "case BOOL: switch(r.type) {\n";
-			stream << "\t\t\t" << "case BOOL  : return " << name << "_impl(l.val.b, r.val.b);" "\n";
-			stream << "\t\t\t" << "case CHAR  : return " << name << "_impl(l.val.b, r.val.c);" "\n";
-			stream << "\t\t\t" << "case INT   : return " << name << "_impl(l.val.b, r.val.i);" "\n";
-			stream << "\t\t\t" << "case FLOAT : return " << name << "_impl(l.val.b, r.val.f);" "\n";
-			stream << "\t\t" << "}\n";
-			stream << "\t\t" << "case CHAR: switch(r.type) {\n";
-			stream << "\t\t\t" << "case BOOL  : return " << name << "_impl(l.val.c, r.val.b);" "\n";
-			stream << "\t\t\t" << "case CHAR  : return " << name << "_impl(l.val.c, r.val.c);" "\n";
-			stream << "\t\t\t" << "case INT   : return " << name << "_impl(l.val.c, r.val.i);" "\n";
-			stream << "\t\t\t" << "case FLOAT : return " << name << "_impl(l.val.c, r.val.f);" "\n";
-			stream << "\t\t" << "}\n";
-			stream << "\t\t" << "case INT: switch(r.type) {\n";
-			stream << "\t\t\t" << "case BOOL  : return " << name << "_impl(l.val.i, r.val.b);" "\n";
-			stream << "\t\t\t" << "case CHAR  : return " << name << "_impl(l.val.i, r.val.c);" "\n";
-			stream << "\t\t\t" << "case INT   : return " << name << "_impl(l.val.i, r.val.i);" "\n";
-			stream << "\t\t\t" << "case FLOAT : return " << name << "_impl(l.val.i, r.val.f);" "\n";
-			stream << "\t\t" << "}\n";
-			stream << "\t\t" << "case FLOAT: switch(r.type) {\n";
-			stream << "\t\t\t" << "case BOOL  : return " << name << "_impl(l.val.f, r.val.b);" "\n";
-			stream << "\t\t\t" << "case CHAR  : return " << name << "_impl(l.val.f, r.val.c);" "\n";
-			stream << "\t\t\t" << "case INT   : return " << name << "_impl(l.val.f, r.val.i);" "\n";
-			stream << "\t\t\t" << "case FLOAT : return " << name << "_impl(l.val.f, r.val.f);" "\n";
-			stream << "\t\t" << "}\n";
-			stream << "\t" << "}\n";
-			stream << "}\n";
+		void print_arith_dispatch(std::ostream & stream, std::string_view name, Indent indent) {
+			auto indent_1 = indent.in();
+			auto indent_2 = indent_1.in();
+			auto indent_3 = indent_2.in();
+			stream << indent << "Value " << name << "(const Value & l, const Value & r){\n";
+			stream << indent_1 << "switch(l.type) {\n";
+			stream << indent_2 << "case BOOL: switch(r.type) {\n";
+			stream << indent_3 << "case BOOL  : return " << name << "_impl(l.val.b, r.val.b);" "\n";
+			stream << indent_3 << "case CHAR  : return " << name << "_impl(l.val.b, r.val.c);" "\n";
+			stream << indent_3 << "case INT   : return " << name << "_impl(l.val.b, r.val.i);" "\n";
+			stream << indent_3 << "case FLOAT : return " << name << "_impl(l.val.b, r.val.f);" "\n";
+			stream << indent_2 << "}\n";
+			stream << indent_2 << "case CHAR: switch(r.type) {\n";
+			stream << indent_3 << "case BOOL  : return " << name << "_impl(l.val.c, r.val.b);" "\n";
+			stream << indent_3 << "case CHAR  : return " << name << "_impl(l.val.c, r.val.c);" "\n";
+			stream << indent_3 << "case INT   : return " << name << "_impl(l.val.c, r.val.i);" "\n";
+			stream << indent_3 << "case FLOAT : return " << name << "_impl(l.val.c, r.val.f);" "\n";
+			stream << indent_2 << "}\n";
+			stream << indent_2 << "case INT: switch(r.type) {\n";
+			stream << indent_3 << "case BOOL  : return " << name << "_impl(l.val.i, r.val.b);" "\n";
+			stream << indent_3 << "case CHAR  : return " << name << "_impl(l.val.i, r.val.c);" "\n";
+			stream << indent_3 << "case INT   : return " << name << "_impl(l.val.i, r.val.i);" "\n";
+			stream << indent_3 << "case FLOAT : return " << name << "_impl(l.val.i, r.val.f);" "\n";
+			stream << indent_2 << "}\n";
+			stream << indent_2 << "case FLOAT: switch(r.type) {\n";
+			stream << indent_3 << "case BOOL  : return " << name << "_impl(l.val.f, r.val.b);" "\n";
+			stream << indent_3 << "case CHAR  : return " << name << "_impl(l.val.f, r.val.c);" "\n";
+			stream << indent_3 << "case INT   : return " << name << "_impl(l.val.f, r.val.i);" "\n";
+			stream << indent_3 << "case FLOAT : return " << name << "_impl(l.val.f, r.val.f);" "\n";
+			stream << indent_2 << "}\n";
+			stream << indent_1 << "}\n";
+			stream << indent << "}\n";
 			stream << "\n";
 		}
 
@@ -153,16 +159,20 @@ namespace ltn::c::trans::cxx {
 		void print_unary(
 			std::ostream & stream,
 			std::string_view name,
-			std::string_view op) {
+			std::string_view op,
+			Indent indent) {
+
+			auto indent_1 = indent.in();
+			auto indent_2 = indent_1.in();
 			
-			stream << "Value " << name << "(const Value & x) {\n";
-			stream << "\t" << "switch(x.type) {\n";
-			stream << "\t\t" << "case BOOL : return value_int(-static_cast<std::int64_t>(x.val.b));\n";
-			stream << "\t\t" << "case CHAR : return value_int(-static_cast<std::int64_t>(x.val.c));\n";
-			stream << "\t\t" << "case INT  : return value_int(-static_cast<std::int64_t>(x.val.i));\n";
-			stream << "\t\t" << "case FLOAT: return value_float(-static_cast<double>(x.val.f));\n";
-			stream << "\t" << "}\n";
-			stream << "}\n";
+			stream << indent << "Value " << name << "(const Value & x) {\n";
+			stream << indent_1 << "switch(x.type) {\n";
+			stream << indent_2 << "case BOOL : return value_int(-static_cast<std::int64_t>(x.val.b));\n";
+			stream << indent_2 << "case CHAR : return value_int(-static_cast<std::int64_t>(x.val.c));\n";
+			stream << indent_2 << "case INT  : return value_int(-static_cast<std::int64_t>(x.val.i));\n";
+			stream << indent_2 << "case FLOAT: return value_float(-static_cast<double>(x.val.f));\n";
+			stream << indent_1 << "}\n";
+			stream << indent << "}\n";
 			stream << "\n";
 		}
 	}
@@ -171,43 +181,43 @@ namespace ltn::c::trans::cxx {
 		std::ostringstream oss;
 		
 		print_includes(oss);
+		Indent indent_ns {1};
 
 		oss << "namespace ltn {\n";
 
-		print_value_type_enum(oss);
-		print_value_struct(oss);
-		print_value_null(oss);
-		print_value_xyz(oss, "bool", "BOOL", "bool", "b");
-		print_value_xyz(oss, "char", "CHAR", "std::uint8_t", "c");
-		print_value_xyz(oss, "int", "INT", "std::int64_t", "i");
-		print_value_xyz(oss, "float", "FLOAT", "double", "f");
+		print_value_type_enum(oss, indent_ns);
+		print_value_struct(oss, indent_ns);
+		print_value_null(oss, indent_ns);
+		print_value_xyz(oss, indent_ns, "bool", "BOOL", "bool", "b");
+		print_value_xyz(oss, indent_ns, "char", "CHAR", "std::uint8_t", "c");
+		print_value_xyz(oss, indent_ns, "int", "INT", "std::int64_t", "i");
+		print_value_xyz(oss, indent_ns, "float", "FLOAT", "double", "f");
 
-		print_wrapped_operator(oss, "add", "+");
-		print_arith_dispatch(oss, "add");
+		print_wrapped_operator(oss, "add", "+", indent_ns);
+		print_arith_dispatch(oss, "add", indent_ns);
 
-		print_wrapped_operator(oss, "sub", "-");
-		print_arith_dispatch(oss, "sub");
+		print_wrapped_operator(oss, "sub", "-", indent_ns);
+		print_arith_dispatch(oss, "sub", indent_ns);
 
-		print_wrapped_operator(oss, "mlt", "*");
-		print_arith_dispatch(oss, "mlt");
+		print_wrapped_operator(oss, "mlt", "*", indent_ns);
+		print_arith_dispatch(oss, "mlt", indent_ns);
 
-		print_wrapped_operator(oss, "div", "/");
-		print_arith_dispatch(oss, "div");
+		print_wrapped_operator(oss, "div", "/", indent_ns);
+		print_arith_dispatch(oss, "div", indent_ns);
 		
-		print_unary(oss, "neg", "-");
+		print_unary(oss, "neg", "-", indent_ns);
 
 		oss << "}\n";
 
 		oss << "namespace fx {\n";
-		Indent indent {1};
 		for(const auto & fx : program.functions) {
-			oss << indent;
+			oss << indent_ns;
 			print_function_header(*fx, oss);
 			oss << ";}\n";
 		}
 		oss << "\n";
 		for(const auto & fx : program.functions) {
-			oss << transpile_c_functional(*fx, indent);
+			oss << transpile_c_functional(*fx, indent_ns);
 		}
 		oss << "}\n";
 		
