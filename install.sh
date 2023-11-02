7bdb4a527dd7653d26912e97a06e92dc1b143734
Jene Litsch <litsch.jene@gmail.com>
2023-11-02 07:40:29 +0100

    Fix: install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo cp bin/ltn /usr/local/bin/ltn
+sudo cp bin/ltn /usr/local/bin/litan
 sudo cp bin/ltnc /usr/local/bin/ltnc
 sudo cp bin/ltnvm /usr/local/bin/ltnvm
 sudo cp bin/ltninfo /usr/local/bin/ltninfo
